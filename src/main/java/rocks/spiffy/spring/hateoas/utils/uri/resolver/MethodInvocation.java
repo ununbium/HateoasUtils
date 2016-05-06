e6bc74353e361963f633859992e3d8018f272046
Andy Hill <andy.hill.dev@gmail.com>
2016-05-06 02:41:41 +0100

    Added missing class from last commit

---
+++ src/main/java/rocks/spiffy/spring/hateoas/utils/uri/resolver/MethodInvocation.java
@@ -0,0 +1,22 @@
+package rocks.spiffy.spring.hateoas.utils.uri.resolver;
+
+import lombok.Getter;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author Andrew Hill
+ */
+@Getter
+public class MethodInvocation {
+
+    private final Class<?> targetClass;
+    private final Method targetMethod;
+    private final Object[] parameters;
+
+    public MethodInvocation(Class<?> targetClass, Method targetMethod, Object[] parameters) {
+        this.targetClass = targetClass;
+        this.targetMethod = targetMethod;
+        this.parameters = parameters;
+    }
+}
