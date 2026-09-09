eb176af6d673680fafa24bb6beaa67ecdca38fc1
Réda Housni Alaoui <reda-alaoui@hey.com>
2026-09-09 20:41:03 +0200

    Path variable values include the next query param (#23)

--- src/test/java/rocks/spiffy/spring/hateoas/utils/uri/resolver/ControllerUriResolverTest.java
+++ src/test/java/rocks/spiffy/spring/hateoas/utils/uri/resolver/ControllerUriResolverTest.java
@@ -1,5 +1,15 @@
 package rocks.spiffy.spring.hateoas.utils.uri.resolver;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -9,14 +19,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import rocks.spiffy.spring.hateoas.utils.DummyController;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 /**
  * @author Andrew Hill
@@ -98,6 +100,34 @@ public class ControllerUriResolverTest
 
         //then
         assertFalse(identifier.isPresent());
+    }
+
+    @Test
+    public void testPathVariableFollowedByQueryParamDirect() {
+        //given
+        ControllerUriResolver on = ControllerUriResolver.on(
+                methodOn(DummyController.class).download(null, null));
+
+        //when
+        Optional<String> id = on.resolve("http://localhost/dummy/download/5?token=xyz", "id");
+
+        //then
+        assertTrue(id.isPresent());
+        assertThat(id.get(), is("5"));
+    }
+
+    @Test
+    public void testPathVariableFollowedByQueryParam() {
+        //given
+        ControllerUriResolver on = ControllerUriResolver.on(
+                methodOn(DummyController.class).download(null, null));
+
+        //when
+        Map<String, String> params = on.resolve("http://localhost/dummy/download/5?token=xyz");
+
+        //then
+        assertThat(params.size(), is(1));
+        assertThat(params.get("id"), is("5"));
     }
 
     @Test
